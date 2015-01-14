355fae83822bb84faefbabb5f7801e6bf22090e6
orestiss <optourgelis@gmail.com>
2015-01-14 14:04:34 +0200

    Split-image program

---
+++ sequence/splitImage.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	int Nx,Ny;
+
+	if( argc != 3)
+	{
+		printf("expecting two arguments\n");
+	}
+	
+	Nx = argv[1];
+	Ny = argv[2];
+
+	
+}
